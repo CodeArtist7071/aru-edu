c64b3694c917b0bab2bd5a18ca13b448cdc557a2
codeartist7071 <artistproysn@outlook.com>
2026-07-05 12:42:40 +0530

    adding design

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.vt5c7q0ntqg"
+    "revision": "0.ejf6bfnnnmo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- client/src/layouts/UserPanelLayout.tsx
+++ client/src/layouts/UserPanelLayout.tsx
@@ -491,7 +491,7 @@ export default function UserPanelLayout() {
         <div
           ref={scrollRef}
           key={location.pathname}
-          className="flex-1 bg-surface-container-low h-full custom-scrollbar p-0 lg:px-10"
+          className="flex-1 bg-surface-container-low overflow-y-auto h-full custom-scrollbar p-0 lg:px-10"
         >
           <Outlet />
         </div>
